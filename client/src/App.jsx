d5ddc7f632408b174dbe8fe59ac98aa1d3f1543b
AAlejof <scaramouch11@gmail.com>
2023-03-21 19:58:24 -0300

    add imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import heliki from './assets/heliki.gif';
 import './App.css';
+
+axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
   return (
