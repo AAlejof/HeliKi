ef3c4278c4490e7868d8da9353d06e11b38e47ab
AAlejof <scaramouch11@gmail.com>
2023-03-22 17:54:05 -0300

    import components - create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import heliki from './assets/heliki.gif';
+import Landing from './components/Landing/Landing'
+import Home from './components/Home/Home'
+import Tips from './components/Tips/Tips'
+import Admin from './components/Admin/Admin'
 import './App.css';
 
 axios.defaults.baseURL = 'http://localhost:3001';
@@ -10,7 +14,13 @@ axios.defaults.baseURL = 'http://localhost:3001';
 function App() {
   return (
     <div className="App">
-        <img src={heliki} className="App-logo" alt="logo" />     
+        <img src={heliki} className="App-logo" alt="logo" />
+        <Routes>
+          <Route path={'/'} element={<Landing/>}/>
+          <Route path={'/home'} element={<Home/>}/>
+          <Route path={'/tips'} element={<Tips/>}/>
+          <Route path={'/admin'} element={<Admin/>}/>
+        </Routes>     
     </div>
   );
 }
