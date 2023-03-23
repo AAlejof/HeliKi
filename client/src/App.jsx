eaa814767fd368dcb866c083ccc7df74fc45c0e0
AAlejof <scaramouch11@gmail.com>
2023-03-22 22:46:30 -0300

    add footer and navbar render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,39 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import axios from 'axios';
 //import { useEffect } from 'react';
 //import { useDispatch, useSelector } from 'react-redux';
+import Navbar from './components/Navbar/Navbar';
 import Landing from './components/Landing/Landing'
 import Home from './components/Home/Home'
 import Tips from './components/Tips/Tips'
 import Admin from './components/Admin/Admin'
+import Footer from './components/Footer/Footer'
 import './App.css';
 
 axios.defaults.baseURL = 'http://localhost:3001';
 
 function App() {
+  const location = useLocation();
+  const { pathname } = location;
+
+  const boolAddComponent =
+  pathname === '/home' ||
+  pathname === '/tips' ||
+  pathname === '/admin';
+
   return (
     <div className="App">
         
+        {boolAddComponent && <Navbar />}
+
         <Routes>
           <Route path={'/'} element={<Landing/>}/>
           <Route path={'/home'} element={<Home/>}/>
           <Route path={'/tips'} element={<Tips/>}/>
           <Route path={'/admin'} element={<Admin/>}/>
-        </Routes>     
+        </Routes>  
+
+        {boolAddComponent && <Footer />}   
     </div>
   );
 }
