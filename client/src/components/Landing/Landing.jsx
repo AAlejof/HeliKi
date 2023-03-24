47a1cc027e696f99f2dcfaeef8c39ce6c51037b9
AAlejof <scaramouch11@gmail.com>
2023-03-24 13:11:53 -0300

    "add link to home"

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,8 +6,9 @@ const Landing = () => {
     return(
         <>
             <div className={style.landingDiv}>
+                
                 <p className={style.pLandingDiv}>DEUTSCH</p>
-                <p className={style.pLandingDiv}>ESPAÑOL</p>
+                <p className={style.pLandingDiv}><Link to='/home'>ESPAÑOL</Link></p>
                 <p className={style.pLandingDiv}>한국어</p>
             </div>
             
