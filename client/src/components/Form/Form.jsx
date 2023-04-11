0b7da3ab8aaf0ed792b1557bfc0b385b3cad7929
AAlejof <scaramouch11@gmail.com>
2023-04-11 20:12:39 -0300

    add basic imports

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {} from '../../redux/actions/actions';
+import Validate from './Validations';
+import swal from 'sweetalert';
+import { useNavigate, Link } from 'react-router-dom';
+
+
+export default function Form() {
+
+
+};
