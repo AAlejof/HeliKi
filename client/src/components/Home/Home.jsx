b4cd906e7da7fee7b2f0dd68f8a7aa32fecc876d
AAlejof <scaramouch11@gmail.com>
2023-04-12 18:26:51 -0300

    add party div

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import style from './Home.module.css';
 import image from '../../assets/heliki.gif';
+import { Link } from 'react-router-dom';
+import Form from '../Form/Form';
 
 const Home = () => {
     return(
@@ -23,10 +25,22 @@ const Home = () => {
                 <br/>Heli & Kiki
             </p>
         </div>
-        <div className={style.ceremonyDiv}>
+        <div className={style.eventsDiv}>
             <h3 className={style.h3Div}>CEREMONIA</h3>
-            <p  className={style.pCeremonyDiv}>Sábado <br/> 18.11.2023 <br/><br/>19:00 h <br/><br/>Iglesia So-Mang <br/>(Morón 3744, Flores. CABA)</p>
-            <button className={style.smallButton}>GOOGLE MAPS</button>
+            <p  className={style.pEventsDiv}>Sábado <br/> 18.11.2023 <br/><br/>19:00 h <br/><br/>Iglesia So-Mang <br/>(Morón 3744, Flores, CABA)</p>
+            <Link to='https://goo.gl/maps/BXHWCvkLRu8VpnDf9'>
+                <button className={style.smallButton}>GOOGLE MAPS</button>
+            </Link>
+        </div>
+        <div className={style.eventsDiv}>
+            <h3 className={style.h3Div}>FIESTA</h3>
+            <p  className={style.pEventsDiv}>Sábado <br/> 18.11.2023 <br/><br/>21:00 h <br/><br/>Lowlands <br/>(Blanco Encalada 1201,<br/> Belgrano, CABA)</p>
+            <Link to='https://goo.gl/maps/XbMraPt6wvK125gz8'>
+                <button className={style.smallButton}><i class="fa-solid fa-map-location-dot fa-xl"></i></button>
+            </Link>
+        </div>
+        <div>
+            <Form/>
         </div>
         </>
     )
