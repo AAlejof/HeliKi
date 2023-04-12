af362f224e184eb85dd672a45f4fcc331ed2386c
AAlejof <scaramouch11@gmail.com>
2023-04-12 18:50:49 -0300

    add form basic structure

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,12 +1,24 @@
+import style from './Form.module.css';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {} from '../../redux/actions/actions';
+//import {} from '../../redux/actions/actions';
 import Validate from './Validations';
 import swal from 'sweetalert';
 import { useNavigate, Link } from 'react-router-dom';
 
 
 export default function Form() {
-
+    return(
+        <>
+            <div>
+                <div className={style.rsvpDiv}>
+                    <h3 className={style.h3Div}>RSVP</h3>
+                    <p  className={style.pRsvpDiv}>ASISTENCIA</p>
+                    
+                    <button className={style.smallButton}>ENVIAR</button>
+                </div>
+            </div>
+        </>
+    )
 
 };
