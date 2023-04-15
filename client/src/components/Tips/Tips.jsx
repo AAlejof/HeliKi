840294c56f818a082f9ebaf5280d0336ef70ab9c
AAlejof <scaramouch11@gmail.com>
2023-04-14 23:04:39 -0300

    add basic structure

--- client/src/components/Tips/Tips.jsx
+++ client/src/components/Tips/Tips.jsx
@@ -1,11 +1,19 @@
-// import style from './Tips.module.css';
+import style from './Tips.module.css';
 // import { Link } from 'react-router-dom';
 import React from 'react';
 
 const Tips = () => {
     return(
         <>
+            <div className={style.sectionsDiv}>
+                <h3 className={style.h3Div}>TIPS GENERALES </h3>
+                <h3 className={style.h3Div}>TRANSPORTE</h3>
+                <h3 className={style.h3Div}>MAPA DE LA CIUDAD</h3>
+                <h3 className={style.h3Div}>VIAJES POR EL CONTINENTE</h3>
+            </div>
+            <div>
 
+            </div>
         
         </>
     )
