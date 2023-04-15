ed568dfe19f356c853aed9038a250ba43a2ed80e
AAlejof <scaramouch11@gmail.com>
2023-04-14 23:12:36 -0300

    add back button

--- client/src/components/Tips/Tips.jsx
+++ client/src/components/Tips/Tips.jsx
@@ -1,22 +1,25 @@
 import style from './Tips.module.css';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 const Tips = () => {
-    return(
-        <>
-            <div className={style.sectionsDiv}>
-                <h3 className={style.h3Div}>TIPS GENERALES </h3>
-                <h3 className={style.h3Div}>TRANSPORTE</h3>
-                <h3 className={style.h3Div}>MAPA DE LA CIUDAD</h3>
-                <h3 className={style.h3Div}>VIAJES POR EL CONTINENTE</h3>
-            </div>
-            <div>
-
-            </div>
-        
-        </>
-    )
-}
+	return (
+		<>
+			<div className={style.sectionsDiv}>
+				<h3 className={style.h3Div}>TIPS GENERALES </h3>
+				<h3 className={style.h3Div}>TRANSPORTE</h3>
+				<h3 className={style.h3Div}>MAPA DE LA CIUDAD</h3>
+				<h3 className={style.h3Div}>VIAJES POR EL CONTINENTE</h3>
+			</div>
+			<div>
+				<Link to="/home">
+					<button className={style.smallButton}>
+						VOLVER
+					</button>
+				</Link>
+			</div>
+		</>
+	);
+};
 
 export default Tips;
