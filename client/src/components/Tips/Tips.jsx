1159db458db89a47cc35a3d04dafa905be50c1d9
AAlejof <scaramouch11@gmail.com>
2023-04-17 21:19:41 -0300

    add conditional rendering to tips div

--- client/src/components/Tips/Tips.jsx
+++ client/src/components/Tips/Tips.jsx
@@ -1,16 +1,28 @@
 import style from './Tips.module.css';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Tips = () => {
+
+	const [showContent, setShowContent] = useState(false);
+
+	const toggleContent = () => {
+		setShowContent(!showContent);
+	};
+
 	return (
 		<>
 			<div className={style.sectionsDiv}>
+				<div>
 				<div className={style.tipsDiv}>
 					<p className={style.h3Div}>TIPS GENERALES</p>
-					<p className={style.faDiv}>
-						<i class="fa-light fa-plus fa-2xl"></i>
+					<p className={style.faDiv} onClick={toggleContent}>
+						{ (showContent=== false) ? (<i class="fa-light fa-plus fa-2xl"></i>) 
+						: (<i class="fa-solid fa-chevron-up fa-xl"></i>) }
 					</p>
+				</div>
+				{(showContent===true) ? <div><p>No se mueran</p></div> : <div></div> }
 				</div>
 				<div className={style.tipsDiv}>
 					<p className={style.h3Div}>TRANSPORTE</p>
