3335287e9b8f13c48f0277f89346e161af91cf04
Alejo Fariña <scaramouch11@gmail.com>
2023-04-25 22:18:47 -0300

    import languageDetector

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,8 +1,16 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18next
 .use(initReactI18next)
 .init({
-    falbackLng: 'en'
+    falbackLng: 'en',
+    resources: {
+        en: {
+            translation: {
+                
+            }
+        }
+    }
 })
