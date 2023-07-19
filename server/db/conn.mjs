4575ecdd19f64ae7e39172f75d5a59a83e3e111a
Alejo Fariña <scaramouch11@gmail.com>
2023-07-19 19:01:42 -0300

    change MONGODB_URI

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = process.env.ATLAS_URI || "";
+//const connectionString = process.env.ATLAS_URI || "";
+const connectionString = process.env.MONGODB_URI || "";
 
 const client = new MongoClient(connectionString);
 
