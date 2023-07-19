20fb291d1a1eeebe5b8510d8d02d1f1828da0404
Alejo Fariña <scaramouch11@gmail.com>
2023-07-18 22:02:53 -0300

    delete unused link

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import style from './Footer.module.css';
-import { Link } from 'react-router-dom';
 import React from 'react';
 
 const Footer = () => {
@@ -7,17 +6,16 @@ const Footer = () => {
 		<>
 			<div className={style.footerDiv}>
 				<div>
-					<Link>
-						<p className={style.pFooterDiv}>Wedding invitation</p>
-					</Link>
+
+					<p className={style.pFooterDiv}>Wedding invitation</p>
 				</div>
 				<div>
 					<p className={style.pFooterDiv}>18.11.2023</p>
 				</div>
 			</div>
-				<div className={style.devDiv}>
-					<p> <a href="https://github.com/AAlejof">Developed by Aalejof</a> </p>
-				</div>
+			<div className={style.devDiv}>
+				<p> <a href="https://github.com/AAlejof">Developed by Aalejof</a> </p>
+			</div>
 		</>
 	);
 };
